e08ff3412c7e97096201f53c5760aa9db7aa2c15
Holo314 <yuval.paz1@mail.huji.ac.il>
2023-02-22 00:56:52 +0200

    Change string to multiline

--- src/test/java/test/org/holo/warn/testdata/Overrides.java
+++ src/test/java/test/org/holo/warn/testdata/Overrides.java
@@ -3,48 +3,50 @@ package test.org.holo.warn.testdata;
 public class Overrides {
     // This can't be code because it won't compile (It contains usage of methods classified as "severity = Diagnostic.Kind.ERROR")
     public static final String code =
-            "package test.org.holo.warn.testdata;\n" +
-            "\n" +
-            "import org.holo.warn.compiletime.annotations.Warning;\n" +
-            "\n" +
-            "import javax.tools.Diagnostic;\n" +
-            "\n" +
-            "public abstract class Override {\n" +
-            "    @Warning(severity = Warning.Kind.NOTE, message = \"Override\")\n" +
-            "    public void foo(){}\n" +
-            "\n" +
-            "    @Warning(severity = Warning.Kind.NOTE, message = \"Override\")\n" +
-            "    public void bar(){}\n" +
-            "\n" +
-            "    @Warning(severity = Warning.Kind.NOTE, message = \"Override\")\n" +
-            "    public void corge(){}\n" +
-            "\n" +
-            "    @Warning(severity = Warning.Kind.NOTE, message = \"Override\")\n" +
-            "    abstract void qux();\n" +
-            "\n" +
-            "    public static class Overriding extends Override {\n" +
-            "        @java.lang.Override\n" +
-            "        public void foo() {\n" +
-            "            super.foo();\n" +
-            "        }\n" +
-            "\n" +
-            "        @Warning(severity = Warning.Kind.ERROR, message = \"Overriding\")\n" +
-            "        @java.lang.Override\n" +
-            "        public void bar() {\n" +
-            "            super.bar();\n" +
-            "        }\n" +
-            "\n" +
-            "        @Warning(severity = Warning.Kind.ERROR, message = \"Overriding\")\n" +
-            "        @java.lang.Override\n" +
-            "        void qux() {}\n" +
-            "    }\n" +
-            "\n" +
-            "    public static void main(String[] args) {\n" +
-            "        var x = new Overriding();\n" +
-            "        x.foo();\n" +
-            "        x.bar();\n" +
-            "        x.corge();\n" +
-            "        x.qux();\n" +
-            "    }\n" +
-            "}\n";
+            """
+                    package test.org.holo.warn.testdata;
+
+                    import org.holo.warn.compiletime.annotations.Warning;
+
+                    import javax.tools.Diagnostic;
+
+                    public abstract class Override {
+                        @Warning(severity = Warning.Kind.NOTE, message = "Override")
+                        public void foo(){}
+
+                        @Warning(severity = Warning.Kind.NOTE, message = "Override")
+                        public void bar(){}
+
+                        @Warning(severity = Warning.Kind.NOTE, message = "Override")
+                        public void corge(){}
+
+                        @Warning(severity = Warning.Kind.NOTE, message = "Override")
+                        abstract void qux();
+
+                        public static class Overriding extends Override {
+                            @java.lang.Override
+                            public void foo() {
+                                super.foo();
+                            }
+
+                            @Warning(severity = Warning.Kind.ERROR, message = "Overriding")
+                            @java.lang.Override
+                            public void bar() {
+                                super.bar();
+                            }
+
+                            @Warning(severity = Warning.Kind.ERROR, message = "Overriding")
+                            @java.lang.Override
+                            void qux() {}
+                        }
+
+                        public static void main(String[] args) {
+                            var x = new Overriding();
+                            x.foo();
+                            x.bar();
+                            x.corge();
+                            x.qux();
+                        }
+                    }
+                    """;
 }
